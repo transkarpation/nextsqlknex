8e28ab5d8301dc5b4c68d9e8eebd8e76e9405d83
Boris <transkarpation@gmail.com>
2021-01-16 13:31:01 +0200

    dashboard

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
     connection: {
         host: '127.0.0.1',
         user: 'root',
-        password: '',
+        password: 'root',
         database: 'cookbook'
     },
     debug: true
---
+++ pages/mail/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+
+export default function index() {
+    return (
+        <div>
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
+        </div>
+    )
+}
